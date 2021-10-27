826659e73da757da5c2b1df0b29166e17c2abc3b
Sakil Khan <sakilk130@gmail.com>
2021-10-27 14:12:24 +0600

    complete nav components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Nav from './components/Nav';
 import './styles/App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
 
       {/* Navigation */}
+      <Nav />
       {/* Results */}
     </div>
   );
